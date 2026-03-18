9818da29761e37fb80a90a984784589b9c7864e2
zwsutherland <zwsutherland@gmail.com>
2026-03-18 16:39:31 -0400

    created radial menu

--- scripts/interaction/cooling_rack.gd
+++ scripts/interaction/cooling_rack.gd
@@ -2,15 +2,27 @@ extends InteractableObject
 
 func _ready() -> void:
 	object_name = "Cooling Unit"
-	actions = ["Inspect", "Increase Cooling", "Decrease Cooling"]
+	actions = ["Turn Off", "Turn On", "Inspect"]
 	interaction_range = 150.0
 	super._ready()
 
 func perform_action(action_name: String) -> void:
 	match action_name:
+		"Turn Off":
+			print("Turning off cooling unit")
+			turn_off()
+		"Turn On":
+			print("Turning on cooling unit")
+			turn_on()
 		"Inspect":
 			print("Inspecting cooling unit")
-		"Increase Cooling":
-			print("Increasing cooling")
-		"Decrease Cooling":
-			print("Decreasing cooling")
+			inspect()
+
+func turn_off() -> void:
+	pass
+
+func turn_on() -> void:
+	pass
+
+func inspect() -> void:
+	pass
--- scripts/interaction/router.gd
+++ scripts/interaction/router.gd
@@ -2,15 +2,27 @@ extends InteractableObject
 
 func _ready() -> void:
 	object_name = "Router"
-	actions = ["Inspect", "Reset", "Reroute"]
+	actions = ["Turn Off", "Turn On", "Reboot"]
 	interaction_range = 150.0
 	super._ready()
 
 func perform_action(action_name: String) -> void:
 	match action_name:
-		"Inspect":
-			print("Inspecting router")
-		"Reset":
-			print("Resetting router")
-		"Reroute":
-			print("Rerouting traffic")
+		"Turn Off":
+			print("Turning off router")
+			turn_off()
+		"Turn On":
+			print("Turning on router")
+			turn_on()
+		"Reboot":
+			print("Rebooting router")
+			reboot()
+
+func turn_off() -> void:
+	pass
+
+func turn_on() -> void:
+	pass
+
+func reboot() -> void:
+	pass
