775234a09a66c38e026bfd1c9e3f03e8219c410c
zwsutherland <zwsutherland@gmail.com>
2026-03-26 18:19:50 -0400

    Buy Menu and Placement

--- scripts/interaction/cooling_rack.gd
+++ scripts/interaction/cooling_rack.gd
@@ -1,22 +1,58 @@
 extends InteractableObject
 
+@export var level: int = 1
+
+var sprites_by_level = {
+	1: {
+		"front": preload("res://assets/object_sprites/cooling_rack_1_front.png"),
+		"right": preload("res://assets/object_sprites/cooling_rack_1_right.png"),
+		"back": preload("res://assets/object_sprites/cooling_rack_1_back.png"),
+		"left": preload("res://assets/object_sprites/cooling_rack_1_left.png")
+	},
+	2: {
+		"front": preload("res://assets/object_sprites/cooling_rack_2_front.png"),
+		"right": preload("res://assets/object_sprites/cooling_rack_2_right.png"),
+		"back": preload("res://assets/object_sprites/cooling_rack_2_back.png"),
+		"left": preload("res://assets/object_sprites/cooling_rack_2_left.png")
+	},
+	3: {
+		"front": preload("res://assets/object_sprites/cooling_rack_3_front.png"),
+		"right": preload("res://assets/object_sprites/cooling_rack_3_right.png"),
+		"back": preload("res://assets/object_sprites/cooling_rack_3_back.png"),
+		"left": preload("res://assets/object_sprites/cooling_rack_3_left.png")
+	}
+}
+
 func _ready() -> void:
-	object_name = "Cooling Unit"
+	object_name = "Cooling Rack L" + str(level)
 	actions = ["Turn Off", "Turn On", "Inspect"]
 	interaction_range = 150.0
 	super._ready()
 
+func set_facing(direction: String) -> void:
+	if sprites_by_level.has(level):
+		var sprites = sprites_by_level[level]
+
+		if sprites.has(direction):
+			sprite.texture = sprites[direction]
+		else:
+			print("Missing direction:", direction)
+	else:
+		print("Missing level:", level)
+
 func perform_action(action_name: String) -> void:
 	match action_name:
 		"Turn Off":
-			print("Turning off cooling unit")
+			print("Cooling rack L", level, "off")
 			turn_off()
 		"Turn On":
-			print("Turning on cooling unit")
+			print("Cooling rack L", level, "on")
 			turn_on()
 		"Inspect":
-			print("Inspecting cooling unit")
+			print("Inspecting cooling rack L", level)
 			inspect()
+		_:
+			super.perform_action(action_name)
 
 func turn_off() -> void:
 	pass
